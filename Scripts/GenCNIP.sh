31626879ba903673f21613b2ae53e46238576716
lnc1995 <lnc1995@outlook.com>
2025-08-19 07:53:40 +0800

    Update GenCNIP.sh

--- Scripts/GenCNIP.sh
+++ Scripts/GenCNIP.sh
@@ -1,5 +1,5 @@
 curl -s https://ispip.clang.cn/all_cn_cidr.txt | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP/g' >1.rsc
-curl -s https://ispip.clang.cn/all_cn_cidr.txt >11.rsc
+curl -s https://ispip.clang.cn/all_cn.txt >11.rsc
 curl -s https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP/g' >2.rsc
 curl -s https://raw.githubusercontent.com/metowolf/iplist/master/data/special/china.txt | sed -e 's/^/add address=/g' -e 's/$/ list=CNIP/g' >3.rsc
 sort -u 1.rsc 2.rsc 3.rsc | sed -e '1i/ip firewall address-list \nremove [/ip firewall address-list find list=CNIP]' >CNIP.RSC
